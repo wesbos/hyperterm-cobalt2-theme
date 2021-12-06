2628506db9cb3db70ff54ad1f5209e1ff10dd5d9
Wes Bos <wesbos@gmail.com>
2021-12-06 15:02:52 -0500

    Fix rounded border

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ exports.decorateConfig = config =>
     }
     .hyper_main {
       ${(config.wickedBorder || config.wickedBorder === undefined) &&
-        `border: 2px solid ${config.wickedBorderColor || magenta} !important;`}
+        `border: 2px solid ${config.wickedBorderColor || magenta} !important;
+          border-radius: 10px;
+        `}
     }
 
     .tabs_nav {
